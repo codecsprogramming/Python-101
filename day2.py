c0ad5c3ff1f2f9f6f69a24435dd40157d43f1b81
Vusala <cs.programming@code.edu.az>
2024-11-14 20:05:47 +0400

    Initial commit

---
+++ day2.py
@@ -0,0 +1,70 @@
+# ---------------------------------DAY 2-----------------------------------
+
+# string methods (cont.)
+## lstrip, rstrip
+## lower, upper
+## find, rfind VS index
+## replace
+## quote inside quote - escaping
+
+# -------------------------------------------------------------------------
+
+# encoding, decoding
+## UTF-8
+## Ascii
+## chr/ord
+## bytes(), bytearray()
+## bytes -> hex
+## hexlify
+
+# string formatting
+## f-string
+### {{ in f-string
+### expression in {}
+
+## format
+### named argument
+### Precision {price:.2f}
+### :c :C - unicode | :b :B - binary | :e :E  - scientific format | :x :X - hex | :% - percentage format
+
+# -------------------------------------------------------------------------
+
+# list, tuple, dictionary, set
+## list
+### add item
+### add 2 lists
+### remove
+### pop
+### count
+### copy
+### insert
+### sort
+### reverse method, reversed function
+### del keyword
+### list comprehension
+### str->list, list->str, list->bytes, bytes->list
+
+## tuple (immutable)
+
+## dict
+### add item to dict
+### delete item
+### dynamic key value for dict
+
+## set (mutable, no duplicate, unordered)
+### assign item (!)
+### add item
+### remove item
+
+## dir function
+### trying  methods in different data types
+
+# -------------------------------------------------------------------------
+
+# if/elif/else
+## falsy expressions
+## and or, priority
+## grouping
+## TASK: simple calculator
+## TASK: IP address format validator
+
---
+++ day3.py
@@ -0,0 +1,28 @@
+# ---------------------------------DAY 3-----------------------------------
+
+# loops
+## range()
+## for loop
+### list
+### string
+### bytearray
+### object keys(), values(), items()
+### else
+
+## while loop
+### else
+## TASK: number guess game
+## TASK: password strength checker
+## TASK: caesar cipher
+
+# -------------------------------------------------------------------------
+
+# files
+## File open modes - r, r+, w, w+, a, a+
+## File methods - read, readlines, readline, write, writelines, seek, truncate,
+## reading binary files
+## changing encoding
+## with keyword
+## TASK: log file analyzer script to detect brute-force attack
+## TASK: CTF challenge
+## TASK: file system scanner for sensitive files (e.g., passwords.txt) and insecure permissions
