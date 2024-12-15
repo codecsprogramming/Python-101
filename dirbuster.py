34bb5f229be4b16e316501a0aa208eab69495888
Nasiba Shahverdiyeva <nasiba.shahverdiyeva@code.local>
2024-12-15 14:46:17 +0400

    task

---
+++ dirbuster.py
@@ -0,0 +1,4 @@
+import sys
+
+if len(sys.argv) < 3:
+    print("[+] Usage: python dirbuster.py URL WORDLIST [EXTENSIONS]")
--- task14 - security headers.py
+++ task14 - security headers.py
@@ -1,7 +1,28 @@
-# TASK: security header checker
-# 1. Define a list of security headers checked
-# 2. Send request to the link - user's input
-# 3. Show which headers exist with [+]
-# 4. Show which headers don't exist with [-]
+# # TASK: security header checker
+# # 1. Define a list of security headers checked
+# # 2. Send request to the link - user's input
+# # 3. Show which headers exist with [+]
+# # 4. Show which headers don't exist with [-]
 
-# Additional functionality: Show recommended setting for missing headers
+# # Additional functionality: Show recommended setting for missing headers
+import requests
+
+security_headers = [
+    "X-Frame-Options",
+    "X-XSS-Protection",
+    "X-Content-Type-Options",
+    "Referrer-Policy",
+    "Strict-Transport-Security",
+    "Content-Security-Policy",
+    "Access-Control-Allow-Origin",
+    "Cross-Origin-Opener-Policy"
+]
+
+url = input("URL: ")
+response = requests.get(url)
+print(type(response.headers))
+for header in security_headers:
+    if header in response.headers:
+        print(f"{header} exists: {response.headers[header]}")
+    else:
+        print(f"{header}: not found")
