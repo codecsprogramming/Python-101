06d77cb31f0783a6dd3308cf9d29ca7110c3380b
Nasiba Shahverdiyeva <nasiba.shahverdiyeva@code.local>
2024-12-15 14:51:49 +0400

    task

--- tasks/dirbuster.py
+++ tasks/dirbuster.py
@@ -1,8 +1,10 @@
+# python .\tasks\dirbuster.py https://juice-shop.herokuapp.com wordlist.txt html,php
 import sys
 
 if len(sys.argv) < 3:
     print("[+] Usage: python dirbuster.py URL WORDLIST [EXTENSIONS]")
 
+print(sys.argv)
 url = sys.argv[1]
 wordlist_path = sys.argv[2]
 extensions = sys.argv[3].split(",") if len(sys.argv) == 4 else []
